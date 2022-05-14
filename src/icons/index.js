d33ec52790f72aa632a52d34d7e07d9a1f876c7f
LXJ <1497404080@qq.com>
2022-05-14 13:37:53 +0800

    人力资源初始化

---
+++ src/icons/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+// register globally
+Vue.component('svg-icon', SvgIcon)
+
+const req = require.context('./svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
+
+//以上两行的代码含义就是，将所有的svg目录下的 .svg后缀的文件全都引入到项目中
